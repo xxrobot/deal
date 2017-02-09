7cf856a2092cde91ba353ddf417cded8e7fd42e6
Scott Sasaki <scottsasaki@scottsasaki.local>
2017-02-08 20:37:32 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+app.get('/js/deck.js', function(req, res){
+  res.sendfile('js/deck.js');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
